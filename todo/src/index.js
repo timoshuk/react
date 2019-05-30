cf38c274ee9e9b95ee168f078012acc15cdde522
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-05-30 17:19:01 +0300

    finish 24 leson

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,15 @@
-console.log("aaaaaa");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById("root"));
