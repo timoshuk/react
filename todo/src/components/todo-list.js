b33a00667573f757359a2e9d901992c7764c1657
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-05-31 16:19:09 +0300

    finish 32

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -1,17 +1,18 @@
 import React from "react";
-
 import TodoListItem from "./todo-list-item";
+import "./todo-list.css";
 
 const TodoList = ({ todos }) => {
   const elements = todos.map(item => {
+    const { id, ...itemProps } = item;
     return (
-      <li>
-        <TodoListItem {...item} />
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps} />
       </li>
     );
   });
 
-  return <ul>{elements}</ul>;
+  return <ul className="list-group todo-list">{elements}</ul>;
 };
 
 export default TodoList;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
   const todoData = [
-    { label: "Drink Coffee", important: false },
-    { label: "Make React App", important: true },
-    { label: "Have a lunch", important: false }
+    { label: "Drink Coffee", important: false, id: 1 },
+    { label: "Make React App", important: true, id: 2 },
+    { label: "Have a lunch", important: false, id: 3 }
   ];
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
       <TodoList todos={todoData} />
     </div>
   );
