fb37dfbdfe879d63f4b43345e296648268239bc6
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-08-20 16:58:23 +0300

    24

--- react-app/todo/src/index.js
+++ react-app/todo/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hellow World");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = (
+  <div>
+    <h1>My Todo List 24</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById("root"));
