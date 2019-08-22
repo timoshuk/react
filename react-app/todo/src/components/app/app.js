c446125693757b646057c29f373590790d781210
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-08-22 17:33:35 +0300

    38

--- react-app/todo/src/components/app/app.js
+++ react-app/todo/src/components/app/app.js
@@ -9,9 +9,9 @@ import "./app.css";
 
 const App = () => {
   const todoData = [
-    { label: "Drink Coffy", important: false, id: 1 },
-    { label: "Make APP", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 }
+    { label: "Drink Coffy", id: 1 },
+    { label: "Make APP", id: 2 },
+    { label: "Have a lunch", id: 3 }
   ];
   return (
     <div className="todo-app">
--- react-app/todo/src/components/todo-list-item/todo-list-item.js
+++ react-app/todo/src/components/todo-list-item/todo-list-item.js
@@ -2,28 +2,42 @@ import React, { Comment } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends React.Component {
-  onLabelClick() {
-    alert(`${label} is klick`);
-  }
-  render() {
-    const { label, important = false } = this.props;
+  state = {
+    done: false,
+    important: false
+  };
 
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
+  onLabelClick = () => {
+    this.setState({ done: true });
+  };
+
+  onMarkImportant = () => {
+    this.setState({ important: true });
+  };
+
+  render() {
+    const { label } = this.props;
+
+    const { done, important } = this.state;
+    let classNames = "todo-list-item";
+
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important ";
+    }
+
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
