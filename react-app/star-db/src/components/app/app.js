3efe544ae2715ae4fbe0df58aa1ce427a5cf3dd7
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-09-01 23:10:01 +0300

    100

--- react-app/star-db/src/components/app/app.js
+++ react-app/star-db/src/components/app/app.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ErrorBoundry from '../error-boundry';
-import SwapiService from '../../services/swapi-service';
-import DummySwapiService from '../../services/dummy-swapi-service';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ErrorBoundry from "../error-boundry";
+import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
-import { SwapiServiceProvider } from '../swapi-service-context';
+import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
+import { SwapiServiceProvider } from "../swapi-service-context";
 
-import './app.css';
+import "./app.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { StarshipDetails } from "../sw-components";
 
 export default class App extends Component {
-
   state = {
     swapiService: new SwapiService()
   };
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
-      const Service = swapiService instanceof SwapiService ?
-                        DummySwapiService : SwapiService;
+      const Service =
+        swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
       return {
         swapiService: new Service()
       };
@@ -28,19 +30,44 @@ export default class App extends Component {
   };
 
   render() {
-
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+        <SwapiServiceProvider value={this.state.swapiService}>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
+              <Route
+                path="/"
+                render={() => {
+                  return <h2>Welcome to StarDB</h2>;
+                }}
+                exact
+              />
+              <Route
+                path="/people"
+                render={() => {
+                  return <h2>People</h2>;
+                }}
+              />
+              <Route path="/people" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route
+                path="/starships/:id"
+                render={({ match }) => {
+                  const { id } = match.params;
+                  return <StarshipDetails itemId={id} />;
+                }}
+              />
 
-          </div>
+              {/* 
+              <PeoplePage />
+              <PlanetsPage />
+              <StarshipsPage /> */}
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
