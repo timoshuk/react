6a11790fcc07d25be300fbcb5e6901292a91e9cd
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-09-11 14:21:49 +0300

    65

---
+++ book/src/components/app/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./app.css";
+
+const App = () => {
+  return <h1>APP</h1>;
+};
+
+export default App;
--- book/src/index.js
+++ book/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import App from "./components/app";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const node = document.getElementById("root");
+const root = React.createElement(
+  "div",
+  {},
+  React.createElement(
+    "h1",
+    {},
+    "Hello World!!!",
+    React.createElement(
+      "a",
+      { href: "mailto:o.t@gmail.com" },
+      React.createElement("h1", {}, "React in actions"),
+      React.createElement("em", {}, "...and now it realy is!!!")
+    )
+  )
+);
+ReactDOM.render(root, node);
