3657c920429370bc9258095a11fbb6989a404283
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-09-23 16:43:28 +0300

    add todo-list

--- hooks-todo/src/App.js
+++ hooks-todo/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./components/todo";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { text: "React Hooks in Depth", isComplated: false },
+    { text: "Write Articles @ Medium", isComplated: false },
+    { text: "Share Articles at Reddit", isComplated: false }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
