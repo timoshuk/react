bdb8a86ae04c7702649e84690418175a6b8d4e50
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-09-29 22:32:02 +0300

    dell hoks

--- react-app/redux-sabdbox/src/index.js
+++ react-app/redux-sabdbox/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/counter";
+import App from "./components/app";
 import { Provider } from "react-redux";
 
 import { createStore } from "redux";
