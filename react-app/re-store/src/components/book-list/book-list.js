bf1301badc8725af21869f1ec0df87a0de176cf6
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-10-01 21:49:06 +0300

    121

--- react-app/re-store/src/components/book-list/book-list.js
+++ react-app/re-store/src/components/book-list/book-list.js
@@ -11,15 +11,16 @@ class BookList extends Component {
   }
   render() {
     const { books } = this.props;
+    console.log(books);
     return (
       <ul>
-        {books.map(book => {
+        {/* {books.map(book => {
           return (
             <li key={book.id}>
               <BookListItem book={book} />
             </li>
           );
-        })}
+        })} */}
       </ul>
     );
   }
