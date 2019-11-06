b834bfa8e5ade6fa22bd01b5d2e043cb812f7d3c
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-11-06 14:09:42 +0200

    Finish test comment

--- react_yakobchuk/src/Comment.js
+++ react_yakobchuk/src/Comment.js
@@ -2,14 +2,32 @@ import React, {Component} from "react";
 
 
 export default class Comment extends Component {
-
+state ={
+    showComment: false
+};
     render(){
-
+        const btnData = this.state.showComment ? 'Close' : 'Open';
         return (<div className="coment">
-                    <button>Open</button>
+                    <button onClick={this.toggleShow}>{btnData}</button>
+                    {this.showComment()}
                 </div>);
 
     }
+
+    toggleShow = ()=>{
+        this.setState({showComment: !this.state.showComment});
+    };
+
+    showComment = () =>{
+        if(!this.state.showComment){
+            return false
+        }else{
+            return (<div>
+                <h2>Title</h2>
+                <div>Sam text for comments</div>
+            </div>);
+        }
+    };
 
     
 }
