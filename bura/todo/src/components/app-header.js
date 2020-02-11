1c470439cf09950581c1e670f37a8aa759786858
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-11 12:44:09 +0200

    Finish style todo

--- bura/todo/src/components/app-header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const AppHeader = () => {
-    return (
-        <h1>My Todo List (8 Props)</h1>
-    );
-};
-
-export default AppHeader;
--- bura/todo/src/index.js
+++ bura/todo/src/index.js
@@ -1,24 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffy", important: false, id: 1 },
-    { label: "Learn React", important: true, id: 2 },
-    { label: "Have a lanch", important: false, id: 3 }
-  ];
-
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+import App from "./components/app"
 
 ReactDOM.render(<App />, document.getElementById("root"));
