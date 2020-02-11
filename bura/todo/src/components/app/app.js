724a7f3b558fde28fa0fe5e33d7ce5df66e04251
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-11 22:34:13 +0200

    Set state 7

--- bura/todo/src/components/app/app.js
+++ bura/todo/src/components/app/app.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import TodoList from '../todo-list';
-import ItemStatusFilter from '../item-status-filter';
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
 
-import './app.css';
+import "./app.css";
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Make Awesome App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 }
+    ]
+  };
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => {
+        return el.id === id;
+      });
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      const before = todoData.slice(0, idx);
+      const after = todoData.slice(idx + 1);
+      const newArray = [...before, ...after];
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render() {
+    const { todoData } = this.state;
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={todoData} onDeleted={this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- bura/todo/src/components/todo-list-item/todo-list-item.js
+++ bura/todo/src/components/todo-list-item/todo-list-item.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      done: false
-    };
-  }
+  state = {
+    done: false,
+    important: false
+  };
 
   onLabelClick = () => {
-    console.log(`Click ${this.props.label}`);
-  }
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      };
+    });
+  };
 
-
+  onMarkImportant = () => {
+    this.setState(({ important }) => {
+      return {
+        important: !important
+      };
+    });
+  };
 
   render() {
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
 
-    const { label, important = false } = this.props;
+    let classNames = "todo-list-item";
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
 
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
-        <button type="button"
-          className="btn btn-outline-success btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
+        >
           <i className="fa fa-exclamation" />
         </button>
 
-        <button type="button"
-          className="btn btn-outline-danger btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
+        >
           <i className="fa fa-trash-o" />
         </button>
       </span>
