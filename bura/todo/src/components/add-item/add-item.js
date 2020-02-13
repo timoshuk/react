131fdb5dce87b363f53f181f3beeeae77eed7c10
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-13 17:34:10 +0200

    finish setState 12

--- bura/todo/src/components/add-item/add-item.js
+++ bura/todo/src/components/add-item/add-item.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./add-item.css";
-const AddItem = ({ addItem }) => {
-  return (
-    <div className="add-item mt-2">
-      <button
-        className="btn btn-block btn-danger"
-        onClick={() => {
-          addItem("Test data");
-        }}
-      >
-        Add Item
-      </button>
-    </div>
-  );
+export default class AddItem extends Component {
+
+  state = {
+    label: ''
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addItem(this.state.label);
+
+    this.setState({ label: '' });
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  }
+  render() {
+    return (
+      <form className="add-item mt-2 d-flex" onSubmit={this.onSubmit}>
+        <input type="text" className="form-control mr-2" onChange={this.onLabelChange} placeholder="what Nids to be done" value={this.state.label} />
+        <button
+          className="btn btn-block btn-danger"
+        >
+          Add Item
+        </button>
+      </form>
+    );
+  }
 };
 
-export default AddItem;
