5c6523a30bf9b2373ca755582e65d6049b699e87
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-14 09:12:44 +0200

    Start star-db

---
+++ bura/star-db/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+const Data = () => {
+    return (
+        <div>
+            <h1>Data</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<Data />, document.getElementById('root'));
