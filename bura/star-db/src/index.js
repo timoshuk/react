1fc18c72ee25b25cc1588cdbf4b127991d5639bc
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-14 10:40:36 +0200

    fech 5

--- bura/star-db/src/index.js
+++ bura/star-db/src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}`);
+    }
+
+    const body = await res.json();
+
+    return body;
+};
+
+const dd = getResource('https://swapi.co/api/people/1').then((body) => {
+    console.log(body)
+}).catch((err) => {
+    console.error(err)
+});
+
+
+
+
 const Data = () => {
     return (
         <div>
