35e55eac2bb3da2f982aa3d08f2cce7495b5fdae
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-14 15:25:55 +0200

    Swapi API 6

--- bura/star-db/src/index.js
+++ bura/star-db/src/index.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import ReactDOM from "react-dom";
+class SwapiService {
+    _apiBase = 'https://swapi.co/api';
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
 
-const getResource = async (url) => {
-    const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}`);
+        }
 
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url}`);
+        const body = await res.json();
+
+        return body;
     }
 
-    const body = await res.json();
-
-    return body;
-};
-
-const dd = getResource('https://swapi.co/api/people/1').then((body) => {
-    console.log(body)
-}).catch((err) => {
-    console.error(err)
-});
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
 
 
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
 
 
-const Data = () => {
-    return (
-        <div>
-            <h1>Data</h1>
-        </div>
-    )
+    getPlanet(id) {
+        return this.getResource(`/planet/${id}/`);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+
+    getStarship(id) {
+        return this.getResource(`/starship/${id}/`);
+    }
+
 }
 
-ReactDOM.render(<Data />, document.getElementById('root'));
+
+const sw = new SwapiService();
+
+sw.getAllPeople().then((body) => {
+    console.log(body);
+});
