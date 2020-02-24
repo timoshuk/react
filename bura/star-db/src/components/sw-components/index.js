cb1926a8f6b60adcd3643a00715c0833a8134e91
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-24 17:11:27 +0200

    19

--- bura/star-db/src/components/sw-components/index.js
+++ bura/star-db/src/components/sw-components/index.js
@@ -2,6 +2,7 @@ import PersonDetails from './person-details';
 import PlanetDetails from './planet-details';
 import StarshipDetails from './starship-details';
 
+
 import {
   PersonList,
   PlanetList,
