bc3a68cf1f91d3aefd527c93167986e6a8e2ce98
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-02-25 21:54:36 +0200

    finish hooks

--- react-router-guide/src/App.js
+++ react-router-guide/src/App.js
@@ -1,26 +1,49 @@
-import React, { Fragment } from 'react';
-import './index.css';
+import React, { Fragment } from "react";
+import "./index.css";
 
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 export default function App() {
-  const name = 'John Doe';
+  const name = "John Doe";
+  const isAuthenticates = true;
+
   return (
     <Router>
       <main>
-        <h1>Guarding routes: https://www.freecodecamp.org/news/a-complete-beginners-guide-to-react-router-include-router-hooks/</h1>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to={`/about/${name}`}>About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to={`/about/${name}`}>About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/about/:name" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route render={() => { return <h1>404: page not found</h1> }} />
+          {isAuthenticates ? (
+            <>
+              <Route path="/about/:name" component={About} />
+              <Route path="/contact" component={Contact} />
+            </>
+          ) : (
+            <Redirect to="/" />
+          )}
+          <Route
+            render={() => {
+              return <h1>404: page not found</h1>;
+            }}
+          />
         </Switch>
       </main>
     </Router>
@@ -29,33 +52,50 @@ export default function App() {
 
 // HomePage
 const Home = () => {
-  return (<Fragment>
-    <h1>Home</h1>
-    <FakeText />
-  </Fragment>
-  )
-}
+  return (
+    <Fragment>
+      <h1>Home</h1>
+      <FakeText />
+    </Fragment>
+  );
+};
 
 // AboutPage
-const About = ({ match: { params: { name } } }) => {
-  return (<Fragment>
-    {name !== 'John Doe' ? <Redirect to="/" /> : null}
-    <h1>About {name}</h1>
-    <FakeText />
-  </Fragment>)
-}
+const About = ({
+  match: {
+    params: { name }
+  }
+}) => {
+  return (
+    <Fragment>
+      {name !== "John Doe" ? <Redirect to="/" /> : null}
+      <h1>About {name}</h1>
+      <FakeText />
+    </Fragment>
+  );
+};
 // ContactPage
 const Contact = ({ history }) => {
-  return (<Fragment>
-    <h1>Contact</h1>
-    <button onClick={() => { history.push('/') }}>Go to home</button>
-    <FakeText />
-  </Fragment>)
-}
+  return (
+    <Fragment>
+      <h1>Contact</h1>
+      <button
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Go to home
+      </button>
+      <FakeText />
+    </Fragment>
+  );
+};
 
 const FakeText = () => {
   return (
-    <p>This HTML file is a template.
-    If you open it directly in the browser, you will see an empty page.</p>
-  )
-}
+    <p>
+      This HTML file is a template. If you open it directly in the browser, you
+      will see an empty page.
+    </p>
+  );
+};
