eae265e2eb7ab6a4244ef16822803bcfdbfebb08
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-03-25 13:29:12 +0200

    re-store 17

--- bura/re-store/src/reducers/index.js
+++ bura/re-store/src/reducers/index.js
@@ -1,19 +1,36 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: "Book 1",
+      count: 3,
+      total: 150
+    },
+    {
+      id: 2,
+      name: "Book 2",
+      count: 2,
+      total: 70
+    }
+  ],
+  orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null
       };
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
@@ -21,6 +38,7 @@ const reducer = (state = initialState, action) => {
 
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
