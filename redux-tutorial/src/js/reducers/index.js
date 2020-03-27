536207be6f02f63e9c487790df181de2e7837fd9
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-03-27 13:12:40 +0200

    finish redux tutorial

--- redux-tutorial/src/js/reducers/index.js
+++ redux-tutorial/src/js/reducers/index.js
@@ -1,17 +1,24 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-  articles: []
+  articles: [],
+  remoteArticles: []
 };
 
-function rootRedocer(state = initialState, action) {
+function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+
+  if (action.type === DATA_LOADED) {
     return {
       ...state,
-      articles: [...state.articles, action.payload]
+      remoteArticles: [...state.remoteArticles, ...action.payload]
     };
   }
   return state;
 }
 
-export default rootRedocer;
+export default rootReducer;
