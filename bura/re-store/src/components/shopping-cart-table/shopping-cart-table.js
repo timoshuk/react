28d7d51d7cec8879ac11e3a165c5f5ae68a57b78
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-03-27 17:58:46 +0200

    22

--- bura/re-store/src/components/shopping-cart-table/shopping-cart-table.js
+++ bura/re-store/src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./shopping-cart-table.css";
 import { connect } from "react-redux";
+import {
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart
+} from "../../actions";
 
 const ShoppingCartTable = ({
   items,
@@ -69,18 +74,10 @@ const mapStateToProps = ({ cartItems, orderTotal }) => {
   };
 };
 
-const mapDispatchToProps = () => {
-  return {
-    onIncrease: id => {
-      console.log(`Increase ${id}`);
-    },
-    onDecrease: id => {
-      console.log(`Decrease ${id}`);
-    },
-    onDelete: id => {
-      console.log(`Delete ${id}`);
-    }
-  };
+const mapDispatchToProps = {
+  onIncrease: bookAddedToCart,
+  onDecrease: bookRemovedFromCart,
+  onDelete: allBooksRemovedFromCart
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
