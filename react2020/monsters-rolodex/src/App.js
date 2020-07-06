aea3ccf6ef681fbf3381a547868476fac20efcaf
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-06 23:07:27 +0300

    22

--- react2020/monsters-rolodex/src/App.js
+++ react2020/monsters-rolodex/src/App.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello my name is Oleksandr</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Some text",
+    };
+  }
+
+  changeTitle = () => {
+    this.setState(({ title }) => {
+      return {
+        title: "AAAAAA",
+      };
+    });
+  };
+
+  render() {
+    const { title } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{title}</p>
+          <button
+            onClick={() => {
+              this.changeTitle();
+            }}
+          >
+            Change TXT
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
