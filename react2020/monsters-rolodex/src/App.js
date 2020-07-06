6f10b999c999023a5dd653d3cf2553dd6f804250
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-06 23:47:16 +0300

    26

--- react2020/monsters-rolodex/src/App.js
+++ react2020/monsters-rolodex/src/App.js
@@ -1,38 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: "Some text",
+      monsters: [
+        {
+          name: "Frankinshtein",
+          id: "uid1",
+        },
+        {
+          name: "Dracula",
+          id: "uid2",
+        },
+        {
+          name: "Zombie",
+          id: "uid3",
+        },
+      ],
     };
   }
-
-  changeTitle = () => {
-    this.setState(({ title }) => {
-      return {
-        title: "AAAAAA",
-      };
-    });
-  };
 
   render() {
     const { title } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{title}</p>
-          <button
-            onClick={() => {
-              this.changeTitle();
-            }}
-          >
-            Change TXT
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
