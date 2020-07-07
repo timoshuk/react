f12b9dbdb6ff2150f5300f1f2385ced3bcff8cf2
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-07 16:56:21 +0300

    35

--- react2020/monsters-rolodex/src/App.js
+++ react2020/monsters-rolodex/src/App.js
@@ -7,18 +7,20 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => this.setState({ monsters: users }))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="search monster" onChange={e => { this.setState({ searchField: e.target.value }) }} />
         <CardList monsters={this.state.monsters} />
       </div>
     );
