a740436bed19a251b59367314ba9e95ec773b5cd
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-08 17:12:18 +0300

    48

--- react2020/monsters-rolodex/src/App.js
+++ react2020/monsters-rolodex/src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="search monsters" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
