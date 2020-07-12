d3006f9d4caac3d5813feba709fed1bc5057f79b
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-12 14:18:01 +0300

    73

--- react2020/crwn-clothing/src/App.js
+++ react2020/crwn-clothing/src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import HomePage from "./components/pages/homepage/homepage.component";
+import ShopPage from "./components/pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
