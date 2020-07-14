12e403be9f8b9bc3d1c485099d296810046a3289
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-14 16:31:47 +0300

    80

--- react2020/crwn-clothing/src/App.js
+++ react2020/crwn-clothing/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
-import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx"
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ react2020/crwn-clothing/src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+
+import SignIn from "../../sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
