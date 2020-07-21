800ad1f2e4ec3ff1e641a156f85e7835d1aa1c99
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-07-21 17:11:47 +0300

    105

--- react2020/crwn-clothing/src/index.js
+++ react2020/crwn-clothing/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>
+  ,
   document.getElementById("root")
 );
