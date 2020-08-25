f7732f396f2d5fbed58a75741dec27e763790ed7
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-08-25 22:14:08 +0300

    finish P-Timer

--- 20-projects/p-timer/src/App.js
+++ 20-projects/p-timer/src/App.js
@@ -1,25 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
-
 function padTime(time) {
-  return time.toString().padStart(2, '0');
+  return time.toString().padStart(2, "0");
 }
 
 function App() {
-
-  const [title, setTitle] = useState('Let the countdown begin!!!');
+  const [title, setTitle] = useState("Let the countdown begin!!!");
   const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRuning, setIsRuning] = useState(false);
+  const intervalRef = useRef(null);
 
   function startTimer() {
-    setInterval(() => {
+    if (intervalRef.current !== null) return;
+    setTitle("Youre doing great!");
+    setIsRuning(true);
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) {
-          return timeLeft - 1
+          return timeLeft - 1;
         }
+        resetTimer();
         return 0;
-      })
-    }, 1000)
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current == null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("Keep it up!");
+    setIsRuning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    setTitle("Ready to go another round?");
+    setTimeLeft(25 * 60);
+    setIsRuning(false);
   }
 
   const minutes = padTime(Math.floor(timeLeft / 60));
@@ -35,11 +54,10 @@ function App() {
         <span>{seconds}</span>
       </div>
 
-
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRuning && <button onClick={startTimer}>Start</button>}
+        {isRuning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
