58c0d76ba28838fa9ac96c3aded5c65ceb1a2b92
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-08-25 16:24:54 +0300

    P-Timer 1 finish

--- 20-projects/p-timer/src/App.js
+++ 20-projects/p-timer/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
 
 function App() {
+
+  const [title, setTitle] = useState('Let the countdown begin!!!');
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) {
+          return timeLeft - 1
+        }
+        return 0;
+      })
+    }, 1000)
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+
+      <div className="buttons">
+        <button onClick={startTimer}>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
