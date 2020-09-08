764694f696edc94828f8916c3dd49e4b72f676c0
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-09-08 06:53:33 +0300

    127

--- react2020/crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ react2020/crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = () => {
-  <div className="chackout-item">
-    <div className="image-kontainer">
-      <img src="" alt="Item" />
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="Item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+
+      <div className="remove-button">&#10005;</div>
     </div>
-  </div>;
+  );
 };
 
 export default CheckoutItem;
