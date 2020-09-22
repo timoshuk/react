7da9a1954bd9d8cbbab07024c6e146b24010c145
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-09-22 17:16:17 +0300

    fix Routing bug

--- react2020/crwn-clothing/src/components/pages/collection/collection.component.jsx
+++ react2020/crwn-clothing/src/components/pages/collection/collection.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { component, connect } from "react-redux";
+import { connect } from "react-redux";
 import { selectCollection } from "../../../redux/shop/shop.selectors";
 import CollectionItem from "../../collection-item/collection-item.component";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
   return (
-    <div className="collection">
-      <h2>Collection Page</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
--- react2020/crwn-clothing/src/components/pages/shop/shop.component.jsx
+++ react2020/crwn-clothing/src/components/pages/shop/shop.component.jsx
@@ -8,11 +8,7 @@ const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route
-        exact
-        path={`${match.path}/:collectionId`}
-        component={CollectionPage}
-      />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
