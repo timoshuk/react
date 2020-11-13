e38859f2aaab290e53259594d848ddfa06c384ad
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-11-13 23:19:34 +0200

    184

--- react2020/crwn-clothing/src/redux/shop/shop.sagas.js
+++ react2020/crwn-clothing/src/redux/shop/shop.sagas.js
@@ -1,8 +1,25 @@
 import { takeEvery } from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectionsSnapshoToMap,
+} from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
   yield console.log("I am fired");
+
+  const collectionRef = firestore.collection("collections");
+  // collectionRef
+  //   .get()
+  //   .then((snapshot) => {
+  //     const collectionsMap = convertCollectionsSnapshoToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
