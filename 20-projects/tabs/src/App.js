39c26c6164e9dbac866f823382f12620765871c1
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-11-13 13:22:37 +0200

    saga

--- 20-projects/tabs/src/App.js
+++ 20-projects/tabs/src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [higlightStyle, setHiglightStyle] = useState({ left: 0, opacity: 0 });
+
+  function moveHiglight(e) {
+    setHiglightStyle({ left: e.nativeEvent.layerX - -150, opacity: 0.3 });
+  }
+
+  function hideHiglight(e) {
+    setHiglightStyle({ left: 0, opacity: 0 });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="browser">
+        <div className="tabs">
+          <div
+            className="tab"
+            onMouseOut={hideHiglight}
+            onMouseMove={moveHiglight}
+          >
+            <div className="higlight" style={higlightStyle} />
+            <a>Home</a>
+          </div>
+          <div
+            className="tab"
+            onMouseOut={hideHiglight}
+            onMouseMove={moveHiglight}
+          >
+            <div className="higlight" style={higlightStyle} />
+            <a>Abaut</a>
+          </div>
+          <div
+            className="tab"
+            onMouseOut={hideHiglight}
+            onMouseMove={moveHiglight}
+          >
+            <div className="higlight" style={higlightStyle} />
+            <a>Features</a>
+          </div>
+        </div>
+        <h1 className="viewport">Pages go here</h1>
+      </div>
     </div>
   );
 }
