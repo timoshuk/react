3203084c200cdd23176adeeab6cf6bfdeb741276
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-07-01 22:16:17 +0300

    Remove old folders

--- bura/bild-sandbox/src/index.js
+++
@@ -1,8 +0,0 @@
-import Log from "./log";
-import Calc from "./calc";
-import img from "./logo.png";
-
-const calc = new Calc();
-const log = new Log();
-
-log.log(calc.add(1, 2, 3));
--- bura/star-db/src/components/hoc-helpers/with-data.js
+++ bura/star-db/src/components/hoc-helpers/with-data.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Spinner from '../spinner';
-import ErrorIndicator from '../error-indicator';
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 const withData = (View) => {
   return class extends Component {
-
     state = {
       data: null,
       loading: true,
-      error: false
+      error: false,
     };
 
     componentDidUpdate(prevProps) {
@@ -24,14 +23,16 @@ const withData = (View) => {
 
     update() {
       this.setState({ loading: true, error: false });
-      this.props.getData()
+      this.props
+        .getData()
         .then((data) => {
           this.setState({
             data,
-            loading: false
+            loading: false,
           });
-        }).catch(() => {
-          this.setState({ error: true, loading: false })
+        })
+        .catch(() => {
+          this.setState({ error: true, loading: false });
         });
     }
 
@@ -43,7 +44,7 @@ const withData = (View) => {
       }
 
       if (error) {
-        return <ErrorIndicator />
+        return <ErrorIndicator />;
       }
 
       return <View {...this.props} data={data} />;
