207ec5ed55087522f280b97d1c24b04f32bae9e6
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-07-14 11:29:53 +0300

    change folder

--- re-store/src/actions/index.js
+++ re-store/src/actions/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:bura/pro-react-redux-master/re-store/120-middleware/src/actions/index.js
 
 const booksRequested = () => {
   return {
@@ -41,7 +40,14 @@ export const allBooksRemovedFromCart = (bookId) => {
   };
 };
 
-const fetchBooks = (bookstoreService, dispatch) => () => {
+const fetchBooksOld = (bookstoreService, dispatch) => () => {
+  dispatch(booksRequested());
+  bookstoreService.getBooks()
+    .then((data) => dispatch(booksLoaded(data)))
+    .catch((err) => dispatch(booksError(err)));
+};
+
+const fetchBooks = (bookstoreService) => () => (dispatch) => {
   dispatch(booksRequested());
   bookstoreService.getBooks()
     .then((data) => dispatch(booksLoaded(data)))
@@ -51,55 +57,3 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
 export {
   fetchBooks
 };
-=======
-const booksRequested = () => {
-  return {
-    type: "FETCH_BOOKS_REQUEST",
-  };
-};
-
-const booksLoaded = (newBooks) => {
-  return {
-    type: "FETCH_BOOKS_SUCCESS",
-    payload: newBooks,
-  };
-};
-
-const booksError = (error) => {
-  return {
-    type: "FETCH_BOOKS_FAILURE",
-    payload: error,
-  };
-};
-
-export const bookAddedToCart = (bookId) => {
-  return {
-    type: "BOOK_ADDED_TO_CART",
-    payload: bookId,
-  };
-};
-
-export const bookRemovedFromCart = (bookId) => {
-  return {
-    type: "BOOK_REMOVED_FROM_CART",
-    payload: bookId,
-  };
-};
-
-export const allBooksRemovedFromCart = (bookId) => {
-  return {
-    type: "ALL_BOOKS_REMOVED_FROM_CART",
-    payload: bookId,
-  };
-};
-
-const fetchBooks = (bookstoreService) => () => (dispatch) => {
-  dispatch(booksRequested());
-  bookstoreService
-    .getBooks()
-    .then((data) => dispatch(booksLoaded(data)))
-    .catch((err) => dispatch(booksError(err)));
-};
-
-export { fetchBooks };
->>>>>>> 3d8f17388da630424a6a1dc1afe3ddd3181604b0:re-store/src/actions/index.js
